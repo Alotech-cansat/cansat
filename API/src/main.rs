6fa72651ba41faf8c2269a90ceb8753919929253
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2023-12-05 16:23:18 +0100

    gert_by_id, get_by_secret_key

--- API/src/main.rs
+++ API/src/main.rs
@@ -1,9 +1,33 @@
-use data::distress_call::*;
+use data::distress_call::DistressCall;
+use repository::idistresscallrepo::*;
 use infrastructure::database::*;
+use rocket::serde::json::Json;
+use rocket::http::Status;
 
-fn main() {
+
+#[macro_use] extern crate rocket;
 
 
-    println!("Hello, world!");
+#[get("/get_by_id/<id>")]
+fn get_by_id(id:i32) -> Result<Json<DistressCall>, Status> {
+    
+    match Database::get_by_id(id){
+        DistressCallFind::Ok(x) => return Ok(Json(x)),
+        _ => Err(Status::NotAcceptable)
+    }
+}
+
+#[get("/get_by_secret_key/<secret_key>")]
+fn get_by_secret_key(secret_key:String) -> Result<Json<DistressCall>, Status> {
+    
+    match Database::get_by_secret_key(secret_key){
+        DistressCallFind::Ok(x) => return Ok(Json(x)),
+        _ => Err(Status::NotAcceptable)
+    }
+}
+
+#[rocket::main]
+async fn main() {
+    rocket::build().mount("/", routes![get_by_id, get_by_secret_key]).launch().await.unwrap();
 }
 
