0c39242daacb7abdf4239fbf15c9bfb12b2f970e
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2023-12-21 15:53:55 +0100

    ground station layout

---
+++ GROUND_STATION/lib.py
@@ -0,0 +1,16 @@
+import requests
+
+def get_by_id(id:int):
+    pass
+
+def get_by_secret_key(secret_ket: str):
+    pass
+
+def new_distress_call(cordinates:str, details:str):
+    pass
+
+def delete(secret_key:str):
+    pass
+
+def update(secret_key:str, cordinates:str, details:str):
+    pass
---
+++ GROUND_STATION/main.py
@@ -0,0 +1,11 @@
+from lib import *
+from typing import List
+class DistressCall:
+    pass
+
+class GroundStation:
+    def __init__(self):
+        self.distress_calls: List[DistressCall] = []
+
+    def run(self):
+        pass
