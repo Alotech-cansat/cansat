4f34f1a20eb26b3109ced0b828987440ad3099ce
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2023-12-23 10:56:46 +0100

    cordinates class, better __repr__ for DistressCall

--- GROUND_STATION/lib.py
+++ GROUND_STATION/lib.py
@@ -1,6 +1,23 @@
 import requests
 from typing import List
 
+
+class Cordinates:
+    def __init__(self, latitude, longitude):
+        self.latitude : float = latitude
+        self.longitude: float = longitude
+
+    @classmethod
+    def from_json(cls, json):
+        obj = cls.__new__(cls)
+        cls.latitude = float(json['latitude'])
+        cls.longitude = float(json['longitude'])
+        return obj
+    
+    def __repr__(self):
+        return f'''\tlatitude : {self.latitude}\n\tlongitude: {self.longitude}'''
+    
+
 class DistressCall:
     
     def __init__(self, id:int, secret_key: str, call_cordinates:str, details:str):
@@ -13,14 +30,14 @@ class DistressCall:
     def from_json(cls,json) -> bool:
         obj = cls.__new__(cls)
         obj.id = json['id']
-        obj.call_cordinates = json['call_cordinates']
+        obj.call_cordinates = Cordinates.from_json(json['call_cordinates'])
         obj.secret_key = json['secret_key']
         obj.details = json['details']
 
         return obj
 
     def __repr__(self):
-        return f'''id: {self.id},\ncall_cordinates: {self.call_cordinates},\ndetails: {self.details}'''
+        return f'''id: {self.id}\ncall_cordinates:\n {self.call_cordinates}\ndetails: {self.details}'''
 
 
 BASE_URL = "http://127.0.0.1:8000"
